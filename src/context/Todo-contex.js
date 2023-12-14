44411ab0369bcc137fa9ef5882ded29e9522fdca
zararafridi <zararafridi9091@gmail.com>
2023-12-14 18:24:02 +0500

    first commit

---
+++ src/context/Todo-contex.js
@@ -0,0 +1,26 @@
+import { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo: "todo message",
+            completed:false
+        }
+    ],
+
+    addTodos:(todo) => {},
+    updateTodo:(id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+
+
+export const TodoProvider = TodoContext.Provider;
